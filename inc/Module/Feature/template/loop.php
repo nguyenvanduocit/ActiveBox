15ee9b7cd71cd3acaa16ab4d5036274ad081e328
nguyenvanduocit <nguyenvanduocit@gmail.com>
2015-09-29 15:38:15 +0700

    implement customizer

--- inc/Module/Feature/template/loop.php
+++ inc/Module/Feature/template/loop.php
@@ -1,9 +1,11 @@
 <?php
-global $wp_query;
+global $wp_query, $diress;
+/** @var \Diress\PostType\Feature $postType */
+$postType = $diress->PostType()->getPostType('feature');
 while($wp_query->have_posts()):$wp_query->the_post();
 	?>
 	<div class="col-md-4 col-sm-6 feature text-center">
-		<span class="icon icon-tools"></span>
+		<?php echo $postType->renderIcon(get_the_ID()); ?>
 		<div class="feature-content">
 			<h5><?php the_title() ?></h5>
 			<?php the_content(); ?>
--- inc/PostType/Feature.php
+++ inc/PostType/Feature.php
@@ -21,9 +21,31 @@ class Feature extends Base{
 		$this->args = array(
 			'supports'=>array( 'title', 'editor', 'thumbnail')
 		);
+		$this->meta_fields = array('feature_icon');
+
 	}
 
 	function init() {
 		add_action( 'init', array( $this, 'registerPostType' ));
+	}
+
+	/**
+	 * Render the icon
+	 * @param $postId
+	 */
+	public function renderIcon($postId){
+		if(has_post_thumbnail($postId)){
+			$post_thumbnail_id = get_post_thumbnail_id( get_the_ID() );
+			$args = array('thumbnail_id'=>$post_thumbnail_id, 'size'=>array(50,60));
+			$imageSrc = $this->getThumbnailSrc($args);
+			echo "<img height='60px' src='{$imageSrc}'>";
+		}
+		else {
+			$iconField = get_post_meta( $postId, 'feature_icon', true );
+			if ( ! $iconField ) {
+				$iconField = 'icon-shield';
+			}
+			echo "<span class='icon {$iconField}'></span>";
+		}
 	}
 }
